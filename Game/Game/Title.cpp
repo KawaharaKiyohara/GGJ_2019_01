fff23aaf3c46d93e243b7cdb241d8fc9c5bed23e
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2019-01-26 20:57:28 +0900

    タイトル組み込み

--- Game/Game/Title.cpp
+++ Game/Game/Title.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "Title.h"
 #include "Game.h"
-
+#include "Fade.h"
 
 Title::Title()
 {
@@ -9,23 +9,36 @@ Title::Title()
 
 Title::~Title()
 {
-	DeleteGO(m_text);
+	DeleteGO(m_spr);
 }
 bool Title::Start()
 {
-	m_text = NewGO<prefab::CFontRender>(0);
-	m_text->SetColor({ 1.0f, 1.0f, 0.0f });
-	m_text->SetText(L"‚ ‚¢‚é‚Ñ[‚Î[‚Ç");
-	m_text->SetPivot({ 0.5f, 0.5f });
-	m_text->SetScale(2.0f);
+	m_spr = NewGO<prefab::CSpriteRender>(0);
+	m_spr->Init(
+		L"sprite/title.dds",
+		GraphicsEngine().Get2DSpaceScreenWidth(),
+		GraphicsEngine().Get2DSpaceScreenHeight()
+	);
 	return true;
 }
 void Title::Update()
 {
-	if (Pad(0).IsPressAnyKey()) {
-		NewGO<Game>(0);
-		DeleteGO(this);
+	switch (m_enStep) {
+	case enStep_Disp:
+		if (Pad(0).IsPressAnyKey()) {
+			m_fade = FindGO<Fade>(GameObjectNames::FADE);
+			m_fade->StartFade({ 0.0f, 0.0f, 0.0f, 1.0f }, 0.5f);
+			m_enStep = enStep_WaitFade;
+		}
+		break;
+	case enStep_WaitFade:
+		if (m_fade->IsFade() == false) {
+			NewGO<Game>(0);
+			DeleteGO(this);
+		}
+		break;
 	}
+
 }
 
 
