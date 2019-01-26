0a737c2a2bba2f6a3968b013781e0a4a70375d7b
komuraathushi <aa>
2019-01-26 15:19:02 +0900

    餌を点滅

--- Game/Game/Feed.h
+++ Game/Game/Feed.h
@@ -18,5 +18,9 @@ private:
 	CVector3 m_position = {0.0f,0.0f,500.0f};
 	CVector3 m_scale = { 5.0f,5.0f,5.0f };
 	prefab::CSkinModelRender* m_skinModelRender = nullptr;
+	float m_timer = 0.0f;
+	float m_timer2 = 0.0f;
+	float m_time = 20.0f;
+	float m_time2 = 10.0f;
 };
 
