8fe3e3708002dac7a8ed0f69dc278b46752f2540
kbc18b16 <kbc18b16@stu.kawahara.ac.jp>
2019-01-27 13:40:28 +0900

    蛇音 鷹音

--- Game/Game/Snake.cpp
+++ Game/Game/Snake.cpp
@@ -52,6 +52,7 @@ void Snake::Move() {
 		if (m_timer >= m_cooltime) {
 			m_timer = 0.0f;
 			m_attack = true;
+			frg_attck2 = false;
 		}
 	}
 	else {
@@ -81,7 +82,14 @@ void Snake::Move() {
 			if (len <=60.0f) {
 				s_Speed =s_Speed * 0.0f;
 				bird->Damage();
-				frg_attck1 = true;
+				if (frg_attck2 == false) {
+					prefab::CSoundSource* ss;
+					ss = NewGO<prefab::CSoundSource>(0);
+					ss->Init(L"sound/poyo.wav");
+					ss->Play(false);
+					frg_attck2 = true;
+				}
+				
 				m_attack = false;
 			}
 		}
